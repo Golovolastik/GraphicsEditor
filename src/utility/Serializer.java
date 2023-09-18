9802a9ba3f8ab2d0988635eb97462e74bf5358c2
Golovolastik <yankovich.aliaksei@gmail.com>
2023-09-18 10:04:58 +0300

    fix: delete unworking methods

--- src/utility/Serializer.java
+++ src/utility/Serializer.java
@@ -1,34 +1,19 @@
 package utility;
 
-import java.io.*;
-
 public class Serializer {
     private FigureList list;
+
     public Serializer(FigureList list) {
         this.list = list;
     }
+
     // Метод для сохранения списка фигур в файл
     public static void save(FigureList list, String filename) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-            oos.writeObject(list);
-            System.out.println("Список фигур успешно сохранен в файл " + filename);
-        } catch (IOException e) {
-            System.err.println("Ошибка при сохранении списка фигур: " + e.getMessage());
-        }
+
     }
 
     // Метод для загрузки списка фигур из файла
     public static FigureList load(String filename) {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            FigureList list = (FigureList) ois.readObject();
-            System.out.println("Список фигур успешно загружен из файла " + filename);
-            return list;
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Ошибка при загрузке списка фигур: " + e.getMessage());
-            return null;
-        }
+        return null;
     }
-
-
-
 }
